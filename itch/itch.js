957a064117290baa27847e78e38600e70f07f49d
pakoito <jueces.jus@gmail.com>
2021-11-11 17:29:41 +0000

    Better itch deploys

--- itch/itch.js
+++ itch/itch.js
@@ -1,15 +1,16 @@
-module.exports = async function beforeAdd() {
+async function start() {
   const util = await import('util');
   const child = await import('child_process');
   const spawnAsync = util.promisify(child.spawn);
   console.log('Updating itch');
-  let result;
   try {
-    result = await spawnAsync('./itch/butler', ['push', './build', 'pakoito/buildmancer:alpha'], { stdio: 'inherit' });
+    await spawnAsync('./itch/butler', ['push', './build', 'pakoito/buildmancer:alpha'], { stdio: 'inherit' });
     console.log('Updated itch');
   } catch (err) {
     console.log(`FAILED updating itch ${err}`);
   }
-  result && console.log(result.stdout);
-  result && console.log(result.stderr);
+
+  return true;
 }
+
+start();
