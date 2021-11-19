67a343213253ac6cd9928b709dab285f36fe2727
pakoito <jueces.jus@gmail.com>
2021-11-19 01:00:36 +0000

    Rename constructor

--- src/utils/effectFunctions.ts
+++ src/utils/effectFunctions.ts
@@ -10,7 +10,7 @@ export type EffectFunRepoIndex = keyof EffectFunRepo;
 export type EffectFunValue<T extends EffectFunRepoIndex> = Parameters<EffectFunRepo[T]>;
 export type EffectFunRepo = typeof EffectRepository;
 
-export const chain = <T>(...funs: Array<ParametrizedFun<T>>): EffectFun<T> =>
+export const effectFun = <T>(...funs: Array<ParametrizedFun<T>>): EffectFun<T> =>
   // TODO check direction of the fold
   funs.reduce((acc, value) => (params) => (origin, play, oldState) => {
     const [newPlay, newState] = acc(params)(origin, play, oldState);
@@ -18,11 +18,11 @@ export const chain = <T>(...funs: Array<ParametrizedFun<T>>): EffectFun<T> =>
   }) as EffectFun<T>;
 
 export const EffectRepository = {
-  'Target:Bleed': chain<{ target: Target; lifespan: number }>(
+  'Target:Bleed': effectFun<{ target: Target; lifespan: number }>(
     ({ target }) => (_origin, play, currentState) => [play, target === 'Player' ? actions.attackPlayer(startState(play), currentState, 1) : actions.attackMonster(startState(play), currentState, target, 3)],
     (params) => (origin, play, currentState) => [play, params.lifespan > 0 ? actions.addEotEffect(currentState, { effect: 'Target:Bleed', origin, parameters: { ...params, lifespan: params.lifespan - 1 } }) : currentState],
   ),
-  'Monster:Summon': chain<{ enemy: MonsterTarget }>(
+  'Monster:Summon': effectFun<{ enemy: MonsterTarget }>(
     ({ enemy }) => (_, play, currentState) => actions.addEnemy(play, currentState, enemies[enemy][0], enemies[enemy][1])
   ),
 };
