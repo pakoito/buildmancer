b0da3b46a3ea362e856f0068d70c0050b3f34de2
pakoito <jueces.jus+nospam@gmail.com>
2022-05-06 20:59:33 +0100

    Better comparison and logging

--- src/tinkerer/buildComparisonCLI.ts
+++ src/tinkerer/buildComparisonCLI.ts
@@ -2,7 +2,7 @@ import minimist from 'minimist';
 import { readFileSync, writeFileSync } from 'fs';
 import prettyjson from 'prettyjson';
 import { build, enemies, randomBuild, randomEnemy, randomPlayer } from '../utils/data';
-import tinker from './tinkerer';
+import tinker, { defaultTinkererOptions } from './tinkerer';
 import { Build, Enemy, EnemyStats, Play } from '../utils/types';
 import { pipe, rangeArr } from '../utils/zFunDump';
 import { makeGameNew } from '../utils/playGame';
@@ -17,14 +17,14 @@ const makeGame = (p: Build, e: Enemy, s: EnemyStats) =>
   );
 
 
-const playGauntlet = (p: Build, iterations: number, gauntlet: [Enemy, EnemyStats][]): ScoredPhenotype<Play>[][] =>
+const playGauntlet = (iterations: number, seed: string, p: Build, gauntlet: [Enemy, EnemyStats][]): ScoredPhenotype<Play>[][] =>
   gauntlet.reduce((acc, current) => {
     const [enemies, enemiesStats] = current;
-    const results = tinker(makeGame(p, enemies, enemiesStats), iterations);
+    const results = tinker(makeGame(p, enemies, enemiesStats), iterations, { playerSeed: seed });
     return [...acc, results];
   }, [] as ScoredPhenotype<Play>[][]);
 
-const start = async ({ builds, encounters, encounterCount, iterations, output, takeTop }: minimist.ParsedArgs) => {
+const start = async ({ builds, encounters, encounterCount, iterations, output, takeTop, seed }: minimist.ParsedArgs) => {
   const players = (JSON.parse(readFileSync(builds).toString()) as BuildConfig[]).map(b => makeBuild(b));
   if (encounters == null && encounterCount == null) {
     console.log(`Please pass one of encounters or encounterCount`);
@@ -33,13 +33,14 @@ const start = async ({ builds, encounters, encounterCount, iterations, output, t
   const gauntlet: [Enemy, EnemyStats][] = encounters != null
     ? encounters.map((e: number) => [enemies[e]])
     : rangeArr(encounterCount).map(_ => randomEnemy());
-  console.log(`\n==========\nCONFIG\n==========\n${prettyjson.render({ iterations })}\n\n==========\nPLAYERS\n==========\n${prettyjson.render(players)}\n==========\nGAUNTLET\n==========\n${prettyjson.render(gauntlet)}\n==========\n`);
 
-  const results = players.map(p => playGauntlet(p, iterations, gauntlet));
-
+  const playerSeed = seed ?? defaultTinkererOptions.playerSeed;
   const topScores = takeTop ?? 5;
-  const scores = results.map(ph => ph.reduce((acc, encounter) => acc + Seq(encounter).take(topScores).reduce((acc, result) => acc + result.score, 0), 0));
+  const config = { iterations, playerSeed, topScores };
+  console.log(`\n==========\nCONFIG\n==========\n${prettyjson.render(config)}\n\n==========\nGAUNTLET\n==========\n${prettyjson.render(gauntlet)}\n==========\n`);
 
+  const results = players.map(p => playGauntlet(iterations, playerSeed, p, gauntlet));
+  const scores = results.map(ph => ph.reduce((acc, encounter) => acc + Seq(encounter).take(topScores).reduce((acc, result) => acc + result.score, 0), 0));
   const winner = scores.reduce(([lead, player], score, idx) => score > lead ? [score, idx] : [lead, player], [0, 0]);
 
   console.log(`\n==========\nSCORES\n==========\n${prettyjson.render(scores)}\n==========\n`);
@@ -47,7 +48,7 @@ const start = async ({ builds, encounters, encounterCount, iterations, output, t
 
   if (output != null) {
     console.log(`Writing to ${output}...`);
-    writeFileSync(output, JSON.stringify(results, null, 2));
+    writeFileSync(output, JSON.stringify({ config, gauntlet, players, results }, null, 2));
   }
 }
 
