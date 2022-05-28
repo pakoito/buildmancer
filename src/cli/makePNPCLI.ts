efd8aaf61feac5950bbb2faf52656ac5e37f0635
pakoito <jueces.jus+nospam@gmail.com>
2022-05-28 22:23:11 +0200

    pnp scaffolding

---
+++ src/cli/makePNPCLI.ts
@@ -0,0 +1,45 @@
+import minimist from 'minimist';
+import { writeFileSync } from 'node:fs';
+import { Item, safeValues } from '../game/types';
+import { build, enemies } from '../game/data';
+
+const start = async ({ out }: minimist.ParsedArgs) => {
+  const outReal = out ?? 'pnp';
+  writeFileSync(
+    `${outReal}/items.json`,
+    JSON.stringify(
+      safeValues(build)
+        .flatMap((v) => safeValues<{ [k: string]: Item }, string>(v))
+        .map((item) => {
+          const passives = item.passives?.join(',') ?? '';
+          const elements = (item.bot ?? [])
+            .map((a) => ({ ...a, when: 'BOT' }))
+            .concat(item.eot?.map((a) => ({ ...a, when: 'EOT' })) ?? [])
+            .concat(item.effects?.map((a) => ({ ...a, when: 'EFFECT' })) ?? []);
+          const effects = elements.reduce(
+            (acc, a, idx) => ({
+              ...acc,
+              [`Name-${idx}`]: a.display,
+              [`Phase-${idx}`]: a.when,
+              [`Description-${idx}`]: a.tooltip,
+              [`Stamina-${idx}`]: a.stamina,
+              [`Priority-${idx}`]: a.priority,
+              [`Ranges-${idx}`]: a.range.join(','),
+              [`Interruptible-${idx}`]: a.interruptible ? 'YES' : 'NO',
+            }),
+            {}
+          );
+          return {
+            display: item.display,
+            ...effects,
+            passives,
+          };
+        }),
+      null,
+      2
+    )
+  );
+  writeFileSync(`${outReal}/enemies.json`, JSON.stringify(enemies, null, 2));
+};
+
+start(minimist(process.argv.slice(2)));
